7933593db51351788cd5dddbfb07682ef200dfc8
Luis Majano <lmajano@gmail.com>
2025-05-28 20:12:55 +0200

    fixing build

--- build/util/debian/install-dependencies.sh
+++ build/util/debian/install-dependencies.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -e
 
-apt-get update && apt-get install --assume-yes \
+apt-get update \
+	&& apt-get install --assume-yes \
                                 apt-utils \
                                 ca-certificates \
                                 curl \
                                 unzip \
-                                jq
-&& apt-get clean \
-&& rm -rf /var/lib/apt/lists/*
+                                jq \
+	&& apt-get clean \
+	&& rm -rf /var/lib/apt/lists/*
