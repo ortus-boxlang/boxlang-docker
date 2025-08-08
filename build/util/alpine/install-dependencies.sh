b27cf0741dc32d23b5af94571f0c6390e327d431
Jon Clausen <jclausen@ortussolutions.com>
2025-08-08 10:06:04 -0400

    add package upgrades to ensure latest security updates are applied

--- build/util/alpine/install-dependencies.sh
+++ build/util/alpine/install-dependencies.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 set -e
-apk update && apk add curl bash unzip jq && rm -f /var/cache/apk/*
+
+apk update
+
+# Update all packages to make sure security updates are applied
+apk upgrade
+
+apk add curl bash unzip jq && rm -f /var/cache/apk/*
